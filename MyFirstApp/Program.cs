f39533078743171d10a96853dde7fad1e4e59a79
KoshevecAP <sghfgdehji@gmail.com>
2025-02-21 22:15:44 +0500

    MyFirstApp

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,47 @@
+﻿  class Program
+  {
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the first number: ");
+        if (!Int32.TryParse(Console.ReadLine(), out var a))
+        {
+            Console.WriteLine("Not a number!");
+            return;
+        }
+
+        Console.Write("Enter the second number: ");
+        if (!Int32.TryParse(Console.ReadLine(), out var b))
+        {
+            Console.WriteLine("Not a number!");
+            return;
+        }
+
+        Console.Write("Enter the '+' '-' '/' '*': ");
+        var s = Console.ReadLine();
+        var boolVar = true;
+        if (s.Length == 0 || s.Length > 1 && !boolVar)
+        {
+            Console.WriteLine("Wrong sign");
+            return;
+        }
+
+        switch (s[0])
+        {
+            case '+':
+                Console.WriteLine("Result of {0} + {1} = {2}", a, b, a + b);
+                break;
+            case '-':
+                Console.WriteLine("Result of {0} - {1} = {2}", a, b, a - b);
+                break;
+            case '/':
+                Console.WriteLine("Result of {0} / {1} = {2}", a, b, a / b);
+                break;
+            case '*':
+                Console.WriteLine("Result of {0} * {1} = {2}", a, b, a * b);
+                break;
+            default:
+                Console.WriteLine("Wrong sign");
+                break;
+        }
+    }
+ }
