b613953c8e0d586ab831a700a0c0a475a8f2b4be
KoshevecAP <sghfgdehji@gmail.com>
2025-02-23 00:01:05 +0500

    Fix

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -16,7 +16,7 @@
             return;
         }
 
-        Console.Write("Enter the '+' '-' '/' '*': ");
+        Console.Write("Enter the '&' '|' '^' : ");
         var s = Console.ReadLine();
         var boolVar = true;
         if (s.Length == 0 || s.Length > 1 && !boolVar)
@@ -27,21 +27,30 @@
 
         switch (s[0])
         {
-            case '+':
-                Console.WriteLine("Result of {0} + {1} = {2}", a, b, a + b);
+            case '&':
+                Console.WriteLine("Result of {0} & {1} = {2}", a, b, a & b);
                 break;
-            case '-':
-                Console.WriteLine("Result of {0} - {1} = {2}", a, b, a - b);
+            case '|':
+                Console.WriteLine("Result of {0} | {1} = {2}", a, b, a | b);
                 break;
-            case '/':
-                Console.WriteLine("Result of {0} / {1} = {2}", a, b, a / b);
-                break;
-            case '*':
-                Console.WriteLine("Result of {0} * {1} = {2}", a, b, a * b);
+            case '^':
+                Console.WriteLine("Result of {0} ^ {1} = {2}", a, b, a ^ b);
                 break;
             default:
                 Console.WriteLine("Wrong sign");
                 break;
         }
+
+        int[] bases = { 2, 10, 16 };
+        int[] numbers = { a, b, a ^ b, a & b, a | b, };
+
+        foreach (int baseValue in bases)
+        {
+            Console.WriteLine("Base {0} Conversion:", baseValue);
+            foreach (short number in numbers)
+            {
+                Console.WriteLine("{0}  -->  {1}",  number, Convert.ToString(number,  baseValue));
+            }         
+        }       
     }
- }
+  }
