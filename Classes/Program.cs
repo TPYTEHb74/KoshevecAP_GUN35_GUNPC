b0d96b2f5c433c0596d1ccdd3432d79c802a7047
KoshevecAP <sghfgdehji@gmail.com>
2025-03-05 17:36:02 +0500

    Classes

---
+++ Classes/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Подготовка к бою");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите начальное здоровье бойца (10-100):");
+            int.TryParse(Console.ReadLine(), out int health); 
+
+
+            var unit = new Unit(name, health);
+        }
+    }
+}
---
+++ Classes/Unit.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Classes;
+
+
+public class Unit
+{
+    private float _health;
+    private float _armour;
+    private float _damage;
+
+
+
+    private float _helm;
+    public string Name { get; }
+
+    public float Health => _health;
+
+    public Unit() : this("Unknown Unit") { }
+
+
+    public Unit(string name, float health)
+    {
+        Name = name;
+        _health = health;
+    }
+
+    public float GetRealHealth()
+    {
+        return Health * (1f + Armour);
+    }
+
+    public float Armour
+    {
+        get { return (float)Math.Round(_armour, (int)0.6); }
+        set
+        {
+            if (value >= 0 || value <= 1)
+            {
+                _armour = value;
+            }
+            else { }
+        }
+    }
+
+    public bool SetDamage()
+    {
+        if (Health <= 0f)
+        {
+            return true;
+            
+        }
+        
+    }
+}
+     
+
+        
+    
+
