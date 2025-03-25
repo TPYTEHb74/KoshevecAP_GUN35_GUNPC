0d7ce025a9d860cc0a0a65dd47b6cedd541524c4
KoshevecAP <sghfgdehji@gmail.com>
2025-03-25 15:37:52 +0500

    GamePrototype

---
+++ GamePrototype/Items/EquipItems/Helm.cs
@@ -0,0 +1,18 @@
+﻿using GamePrototype.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePrototype.Items.EquipItems
+{
+    public class Helm : EquipItem
+    {
+        public Helm(uint maxDurability, string name) : base(maxDurability, name)
+        {
+
+        }
+        public override EquipSlot Slot { get; }
+    }
+}
---
+++ GamePrototype/Utils/UnitFactoryDemoAbstract.cs
@@ -0,0 +1,39 @@
+﻿using GamePrototype.Items.EconomicItems;
+using GamePrototype.Items.EquipItems;
+using GamePrototype.Units;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePrototype.Utils
+{
+    public abstract class UnitFactoryDemoAbstract
+    {
+        public Unit CreatePlayer(string name)
+        {
+            var player = new Player(name, 30, 30, 6);
+            player.AddItemToInventory(new Weapon(10, 15, "Sword"));
+            player.AddItemToInventory(new Armour(10, 15, "Armour"));
+            player.AddItemToInventory(new HealthPotion("Potion"));
+            return player;
+        }
+        public abstract Unit CreateGoblinEnemy();
+    }
+
+    public class UnitFactoryDemoEasy : UnitFactoryDemoAbstract
+    {
+        public override Unit CreateGoblinEnemy() => new Goblin(GameConstants.Goblin, 5, 5, 1);
+    }
+
+    public class UnitFactoryDemoMedium : UnitFactoryDemoAbstract
+    {
+        public override Unit CreateGoblinEnemy() => new Goblin(GameConstants.Goblin, 10, 10, 2);
+    }
+
+    public class UnitFactoryDemoHard : UnitFactoryDemoAbstract
+    {
+        public override Unit CreateGoblinEnemy() => new Goblin(GameConstants.Goblin, 15, 15, 3);
+    }
+}
